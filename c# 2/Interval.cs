8610f8abcaff1e60c7369166af00fe07d4903708
Repanshu <c0911946@mylambton.ca>
2024-02-13 22:49:21 -0500

    second commit

---
+++ c# 2/Interval.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Interval
+{
+    static void Main()
+    {
+        Console.Write("Enter a real number: ");
+        double x = double.Parse(Console.ReadLine());
+
+        bool belongsToI = (x >= 2 && x < 3) || (x >= 0 && x <= 1) || (x >= -10 && x <= -2);
+
+        if (belongsToI)
+        {
+            Console.WriteLine($"x belongs to I");
+        }
+        else
+        {
+            Console.WriteLine($"x does not belong to I");
+        }
+    }
+}
---
+++ c#/RoseBlanches.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class RoseBlanches
+{
+    static void Main()
+    {
+        // Asking the users how much money aunt gave
+        Console.Write("How much did you receive money ($)? ");
+        double totalMoney = double.Parse(Console.ReadLine());
+
+        // Calculate 3/4 for books and supplies
+        double booksAndSupplies = totalMoney * 0.75;
+
+        // Calculate the remaining money for coffee, tickets etc
+        double remainingMoney = totalMoney - booksAndSupplies;
+
+        // Calculate the even split between coffees, Flash Computer journals and subway tickets
+
+        double evenMoney = remainingMoney/3;
+
+        // Calculate the amount of coffees, Flash Computer journals, and subway tickets that we can buy
+        int coffees = (int)(evenMoney / 2);
+        int flashComputers = (int)(evenMoney / 4);
+        int subwayTickets = (int)(evenMoney / 3);
+
+        // Output the result
+        Console.WriteLine($"Book and Supplies: {booksAndSupplies} $");
+        Console.WriteLine($"You can then buy:");
+        Console.WriteLine($"{coffees} coffees");
+        Console.WriteLine($"{flashComputers} Flash Computer Numbers");
+        Console.WriteLine($"{subwayTickets} subway Tickets");
+
+        // Calculate the remaining money for white roses
+        double remainingForRoses = remainingMoney - (coffees * 2 + flashComputers * 4 + subwayTickets *3);
+
+        // Output the remaining money for white roses
+        Console.WriteLine($"and you will have {remainingForRoses} dollars for the white roses.");
+    }
+}
